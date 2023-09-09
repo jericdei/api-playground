b1992ab5b6309219857b7f4bac7f8f3ffbe3d461
Jeric <jericjune.logan.lhoopa@gmail.com>
2023-09-10 02:38:09 +0800

    express: list endpoint, create province

--- javascript/express/index.ts
+++ javascript/express/index.ts
@@ -1,1 +1,37 @@
-console.log("Hello via Bun!");
+import express from 'express'
+import cors from 'cors'
+import { Database } from 'bun:sqlite'
+
+const app = express()
+
+const db = new Database('../../database.sqlite')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/provinces', (req, res) => {
+	const provinces = db.query("SELECT rowid as id, name FROM provinces").all()
+
+	res.send({
+		provinces
+	})
+})
+
+app.post('/provinces', (req, res) => {
+	if (!req.body.name) {
+		res.send({
+			message: "Please enter a name."
+		})
+	}
+
+	db.query("INSERT INTO provinces(name) VALUES (?);").get(req.body.name)
+
+	res.send({
+		message: "Province successfully added."
+	})
+})
+
+app.listen(3000, () => {
+	console.log('Listening on http://localhost:3000')
+})
