d9de2de60497b63f8aab801c8953008874fa116f
Jeric <jericjune.logan.lhoopa@gmail.com>
2023-09-17 12:34:41 +0800

    adonis setup'

---
+++ javascript/adonis/app/Controllers/Http/ProvincesController.ts
@@ -0,0 +1,15 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class ProvincesController {
+  public async index({}: HttpContextContract) {
+
+  }
+
+  public async store({}: HttpContextContract) {}
+
+  public async show({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+}
